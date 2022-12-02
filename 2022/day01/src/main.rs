20a0404ebeb0e8c110c8b9f299c4062987ee4d00
Stefan Zwanenburg <stefan@zwanenburg.info>
2022-12-02 15:22:36 +0100

    Days 1 & 2 -- kinda dirty

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,56 @@
+use std::io;
+
+#[derive(Debug)]
+enum InputLine {
+    Calories(u32),
+    Separator,
+}
+
+fn main() {
+    let mut input = io::stdin()
+        .lines()
+        .map(|result| result.expect("IO error"))
+        .map(|line| {
+            line.parse::<u32>()
+                .map(InputLine::Calories)
+                .unwrap_or(InputLine::Separator)
+        })
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut result, mut elf), input_line| match input_line {
+                InputLine::Calories(n) => (result, {
+                    elf.push(n);
+                    elf
+                }),
+                InputLine::Separator => (
+                    {
+                        result.push(elf);
+                        result
+                    },
+                    vec![],
+                ),
+            },
+        );
+
+    let input = if !input.1.is_empty() {
+        input.0.push(input.1);
+        input.0
+    } else {
+        input.0
+    };
+
+    let silver: u32 = input
+        .iter()
+        .map(|elf| elf.into_iter().sum())
+        .max()
+        .expect("Was there no input?");
+
+    println!("Silver: {}", silver);
+
+    let mut calories_per_elf: Vec<u32> = input.iter().map(|elf| elf.into_iter().sum()).collect();
+    calories_per_elf.sort_by(|a, b| a.cmp(b).reverse());
+
+    let gold: u32 = calories_per_elf.iter().take(3).sum();
+
+    println!("Gold: {}", gold);
+}
---
+++ 2022/day02/src/main.rs
@@ -0,0 +1,131 @@
+use std::io;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum RockPaperScissors {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+use RockPaperScissors::*;
+
+impl RockPaperScissors {
+    fn shape_score(&self) -> u32 {
+        match self {
+            Rock => 1,
+            Paper => 2,
+            Scissors => 3,
+        }
+    }
+
+    fn parse_opponent(str: &str) -> RockPaperScissors {
+        match str {
+            "A" => Rock,
+            "B" => Paper,
+            "C" => Scissors,
+            _ => panic!("Unexpected opponent hand: {}", str),
+        }
+    }
+
+    fn parse_own(str: &str) -> RockPaperScissors {
+        match str {
+            "X" => Rock,
+            "Y" => Paper,
+            "Z" => Scissors,
+            _ => panic!("Unexpected own hand: {}", str),
+        }
+    }
+}
+
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+use Outcome::*;
+
+impl Outcome {
+    fn parse(str: &str) -> Self {
+        match str {
+            "X" => Lose,
+            "Y" => Draw,
+            "Z" => Win,
+            _ => panic!("Unknown outcome detected in input: {}", str),
+        }
+    }
+
+    fn determine_needed_hand(&self, opponent_hand: &RockPaperScissors) -> RockPaperScissors {
+        match self {
+            Draw => *opponent_hand,
+            Win => match opponent_hand {
+                Rock => Paper,
+                Paper => Scissors,
+                Scissors => Rock,
+            },
+            Lose => match opponent_hand {
+                Rock => Scissors,
+                Paper => Rock,
+                Scissors => Paper,
+            },
+        }
+    }
+}
+
+fn score(opponent_hand: &RockPaperScissors, own_hand: &RockPaperScissors) -> u32 {
+    let shape_score = own_hand.shape_score();
+    let win_score = match opponent_hand.cmp(own_hand) {
+        std::cmp::Ordering::Less => 6,
+        std::cmp::Ordering::Equal => 3,
+        std::cmp::Ordering::Greater => 0,
+    };
+
+    shape_score + win_score
+}
+
+impl PartialOrd for RockPaperScissors {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        use std::cmp::Ordering::*;
+
+        Some(match (self, other) {
+            (Rock, Rock) => Equal,
+            (Rock, Paper) => Less,
+            (Rock, Scissors) => Greater,
+            (Paper, Rock) => Greater,
+            (Paper, Paper) => Equal,
+            (Paper, Scissors) => Less,
+            (Scissors, Rock) => Less,
+            (Scissors, Paper) => Greater,
+            (Scissors, Scissors) => Equal,
+        })
+    }
+}
+
+impl Ord for RockPaperScissors {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+fn main() {
+    let (silver, gold) = io::stdin()
+        .lines()
+        .map(|result| result.expect("I/O error"))
+        .map(|line| {
+            let hands: Vec<&str> = line.split_whitespace().collect();
+            assert_eq!(hands.len(), 2, "Hands per line must be two!");
+
+            let opponent = RockPaperScissors::parse_opponent(hands[0]);
+            let own_silver = RockPaperScissors::parse_own(hands[1]);
+            let outcome = Outcome::parse(hands[1]);
+            let own_gold = outcome.determine_needed_hand(&opponent);
+
+            (score(&opponent, &own_silver), score(&opponent, &own_gold))
+        })
+        .fold((0u32, 0u32), |(silver, gold), score| {
+            (silver + score.0, gold + score.1)
+        });
+
+    println!("Silver: {}", silver);
+    println!("Gold: {}", gold);
+}
