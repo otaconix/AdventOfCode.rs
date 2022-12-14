f144fc7c0cffae9cd1b3359fdae39075f4c2fc33
Stefan Zwanenburg <stefan@zwanenburg.info>
2022-12-14 12:01:02 +0100

    Day 13, finally!

---
+++ 2022/day13/src/main.rs
@@ -0,0 +1,100 @@
+use std::{cmp::Ordering, io};
+use types::*;
+
+mod types {
+    use pom::char_class::*;
+    use pom::parser::*;
+    use std::str::FromStr;
+
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub enum Input {
+        List(Vec<Input>),
+        Number(u32),
+    }
+
+    impl FromStr for Input {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            list().parse(s.as_bytes()).map_err(|e| format!("{e}"))
+        }
+    }
+
+    impl PartialOrd for Input {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Input {
+        fn to_list(&self) -> Self {
+            match self {
+                &Input::List(_) => self.clone(),
+                &Input::Number(_) => Input::List(vec![self.clone()]),
+            }
+        }
+    }
+
+    impl Ord for Input {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            use Input::*;
+
+            match (self, other) {
+                (List(xs), List(ys)) => xs.cmp(ys),
+                (List(_), Number(_)) => self.cmp(&other.to_list()),
+                (Number(_), List(_)) => self.to_list().cmp(other),
+                (Number(x), Number(y)) => x.cmp(y),
+            }
+        }
+    }
+
+    fn list<'a>() -> Parser<'a, u8, Input> {
+        let element = call(list) | number();
+
+        sym(b'[') * pom::parser::list(element, sym(b',')).map(|inputs| Input::List(inputs))
+            - sym(b']')
+    }
+
+    fn number<'a>() -> Parser<'a, u8, Input> {
+        is_a(digit).repeat(1..).map(|digits| {
+            Input::Number(
+                digits
+                    .into_iter()
+                    .fold(0u32, |result, digit| result * 10 + digit as u32),
+            )
+        })
+    }
+}
+
+fn main() {
+    let inputs: Vec<_> = io::stdin()
+        .lines()
+        .map(|result| result.expect("I/O error"))
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.parse::<Input>().unwrap())
+        .collect::<Vec<_>>();
+
+    let part_1: usize = (1..)
+        .zip(
+            inputs
+                .chunks(2)
+                .map(|pair| (pair[0].clone(), pair[1].clone())),
+        )
+        .filter(|(_, (a, b))| Ordering::is_le(a.cmp(b)))
+        .map(|(index, _)| index)
+        .sum();
+    println!("Part 1: {part_1}");
+
+    let divider_packets: [Input; 2] = ["[[2]]".parse().unwrap(), "[[6]]".parse().unwrap()];
+    let mut part_2 = divider_packets
+        .iter()
+        .chain(inputs.iter())
+        .collect::<Vec<_>>();
+    part_2.sort();
+    let part_2: usize = (1..)
+        .zip(part_2.iter())
+        .filter(|(_, input)| divider_packets.contains(input))
+        .map(|(index, _)| index)
+        .product();
+    println!("Part 2: {part_2}");
+}
