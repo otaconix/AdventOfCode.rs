1351b5c151bbd9b9eb25f826fcf7de1a3dc3cb45
Stefan Zwanenburg <stefanhetzwaantje@gmail.com>
2022-12-19 14:17:47 +0100

    Day 16, part 2, which is slow as f#@k on the test input

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::io;
 use std::str::FromStr;
 
@@ -104,6 +104,106 @@ fn maximum_possible_pressure_release<'a>(
 
                 new_paths
             })
+            .collect();
+
+        let max_released = paths.iter().map(|path| path.released).max().unwrap();
+
+        paths.retain(|path| path.released > max_released - max_flow_rate);
+    }
+
+    paths.into_iter().map(|path| path.released).max().unwrap()
+}
+
+fn maximum_possible_pressure_release_with_elephant<'a>(
+    start_valve: &'a str,
+    valves: &'a HashMap<String, Valve>,
+    time_left: u32,
+) -> i32 {
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    struct Path<'a> {
+        me: &'a Valve,
+        elephant: &'a Valve,
+        opened: Vec<&'a String>,
+        released: i32,
+        flow_rate: i32,
+    }
+
+    let mut paths = vec![Path {
+        me: &valves[start_valve],
+        elephant: &valves[start_valve],
+        opened: vec![],
+        released: 0,
+        flow_rate: 0,
+    }];
+    let max_flow_rate = valves.values().map(|valve| valve.flow_rate).max().unwrap();
+
+    for _ in 1..=time_left {
+        paths = paths
+            .into_iter()
+            .flat_map(|path| {
+                let mut new_paths = path
+                    .me
+                    .pipe_destinations
+                    .iter()
+                    .map(|destination| Path {
+                        me: &valves[&destination.name],
+                        elephant: path.elephant,
+                        opened: path.opened.clone(),
+                        released: path.released + path.flow_rate,
+                        flow_rate: path.flow_rate,
+                    })
+                    .collect::<Vec<_>>();
+
+                if !path.opened.contains(&&path.me.name) && path.me.flow_rate > 0 {
+                    let mut opened = path.opened.clone();
+                    opened.push(&path.me.name);
+                    new_paths.push(Path {
+                        me: path.me,
+                        elephant: path.elephant,
+                        released: path.released + path.flow_rate,
+                        flow_rate: path.flow_rate + path.me.flow_rate,
+                        opened,
+                    });
+                }
+
+                let max_released = new_paths.iter().map(|path| path.released).max().unwrap();
+
+                new_paths.retain(|path| path.released > max_released - max_flow_rate);
+
+                let new_paths = new_paths
+                    .iter()
+                    .flat_map(|new_path| {
+                        let mut new_elephant_paths = new_path
+                            .elephant
+                            .pipe_destinations
+                            .iter()
+                            .map(|destination| Path {
+                                me: new_path.me,
+                                elephant: &valves[&destination.name],
+                                opened: new_path.opened.clone(),
+                                released: new_path.released,
+                                flow_rate: new_path.flow_rate,
+                            })
+                            .collect::<Vec<_>>();
+
+                        if !new_path.opened.contains(&&new_path.elephant.name)
+                            && new_path.elephant.flow_rate > 0
+                        {
+                            let mut opened = new_path.opened.clone();
+                            opened.push(&new_path.elephant.name);
+                            new_elephant_paths.push(Path {
+                                opened,
+                                flow_rate: new_path.flow_rate + new_path.elephant.flow_rate,
+                                ..*new_path
+                            })
+                        }
+
+                        new_elephant_paths
+                    })
+                    .collect::<Vec<_>>();
+
+                new_paths
+            })
             .collect::<Vec<_>>();
 
         let max_released = paths.iter().map(|path| path.released).max().unwrap();
@@ -114,57 +214,10 @@ fn maximum_possible_pressure_release<'a>(
             .collect::<Vec<_>>();
     }
 
-    println!("Possible paths count: {}", paths.len());
     paths.into_iter().map(|path| path.released).max().unwrap()
 }
 
-fn simplify_graph(
-    start_valve_name: &str,
-    mut valves: HashMap<String, Valve>,
-) -> HashMap<String, Valve> {
-    fn remove_valve(valve_to_remove_name: &str, valves: &mut HashMap<String, Valve>) -> Valve {
-        let removed_valve = valves.remove(valve_to_remove_name).unwrap();
-        let new_destinations = removed_valve.pipe_destinations.clone();
-        valves.values_mut().for_each(|to_update| {
-            to_update.pipe_destinations = to_update
-                .pipe_destinations
-                .iter()
-                .flat_map(|dest| {
-                    if dest.name == valve_to_remove_name {
-                        new_destinations
-                            .iter()
-                            .filter(|new_destination| new_destination.name != to_update.name)
-                            .cloned()
-                            .map(|new_destination| DestinationValve {
-                                steps: new_destination.steps + dest.steps,
-                                ..new_destination
-                            })
-                            .collect()
-                    } else {
-                        vec![dest.clone()]
-                    }
-                })
-                .collect();
-        });
-
-        removed_valve
-    }
-
-    if let Some(zero_flow_rate) = valves
-        .values()
-        .find(|valve| valve.name != start_valve_name && valve.flow_rate == 0)
-        .map(|valve| valve.name.clone())
-    {
-        remove_valve(&zero_flow_rate, &mut valves);
-        simplify_graph(start_valve_name, valves)
-    } else {
-        valves
-    }
-}
-
 fn main() {
-    // let valves = simplify_graph(
-    // "AA",
     let valves = io::stdin()
         .lines()
         .map(|result| result.expect("I/O error"))
@@ -175,9 +228,10 @@ fn main() {
         })
         .map(|valve| (valve.name.clone(), valve))
         .collect::<HashMap<_, _>>();
-    // .collect(),
-    // );
 
     let part_1 = maximum_possible_pressure_release("AA", &valves, 30);
     println!("Part 1: {part_1}");
+
+    let part_2 = maximum_possible_pressure_release_with_elephant("AA", &valves, 26);
+    println!("Part 2: {part_2}");
 }
