e5067f1c1ff580f2fba82762744741154eaa2c91
Stefan Zwanenburg <stefanhetzwaantje@gmail.com>
2023-10-07 12:51:18 +0200

    2022-18 Part 2!
    The idea here is to do a flood fill starting from the outside of the
    droplet to get all 3D coordinates that are on the outside of the
    droplet, then, for each lava bit, keep every neighbor that is within the
    flood filled exterior.

--- years/2022/18/src/main.rs
+++ years/2022/18/src/main.rs
@@ -1,17 +1,47 @@
 use std::collections::HashSet;
 use std::io;
 
-type Coords = (i32, i32, i32);
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+struct LavaBit(i8, i8, i8);
 
-fn neighbors(coords: &Coords) -> [Coords; 6] {
-    [
-        (coords.0 - 1, coords.1, coords.2),
-        (coords.0 + 1, coords.1, coords.2),
-        (coords.0, coords.1 - 1, coords.2),
-        (coords.0, coords.1 + 1, coords.2),
-        (coords.0, coords.1, coords.2 - 1),
-        (coords.0, coords.1, coords.2 + 1),
-    ]
+impl LavaBit {
+    fn neighbors(&self) -> [Self; 6] {
+        [
+            Self(self.0 - 1, self.1, self.2),
+            Self(self.0 + 1, self.1, self.2),
+            Self(self.0, self.1 - 1, self.2),
+            Self(self.0, self.1 + 1, self.2),
+            Self(self.0, self.1, self.2 - 1),
+            Self(self.0, self.1, self.2 + 1),
+        ]
+    }
+}
+
+/// This is basically an implementation of a flood fill algorithm.
+///
+/// By starting outside the droplet (assuming -1,-1,-1 is outside), we flood fill
+/// the 3D space, stopping at lava bits in the droplet.
+fn exterior(lava_bits: &HashSet<LavaBit>) -> HashSet<LavaBit> {
+    let x_range = -1..=lava_bits.iter().map(|lava_bit| lava_bit.0).max().unwrap() + 1;
+    let y_range = -1..=lava_bits.iter().map(|lava_bit| lava_bit.1).max().unwrap() + 1;
+    let z_range = -1..=lava_bits.iter().map(|lava_bit| lava_bit.2).max().unwrap() + 1;
+
+    let mut flooded = HashSet::new();
+    let mut queue = vec![LavaBit(-1, -1, -1)];
+
+    while let Some(n @ LavaBit(x, y, z)) = queue.pop() {
+        if x_range.contains(&x)
+            && y_range.contains(&y)
+            && z_range.contains(&z)
+            && !flooded.contains(&n)
+            && !lava_bits.contains(&n)
+        {
+            queue.extend(n.neighbors());
+            flooded.insert(n);
+        }
+    }
+
+    flooded
 }
 
 fn main() {
@@ -21,21 +51,35 @@ fn main() {
         .map(|line| {
             let nums = line
                 .split(',')
-                .map(|num| num.parse::<i32>().expect("Not a number?"))
+                .map(|num| num.parse::<i8>().expect("Not a number?"))
                 .collect::<Vec<_>>();
 
-            (nums[0], nums[1], nums[2])
+            LavaBit(nums[0], nums[1], nums[2])
         })
         .collect();
 
     let part1 = input
         .iter()
-        .flat_map(|c| {
-            neighbors(c)
+        .flat_map(|lava_bit| {
+            lava_bit
+                .neighbors()
                 .into_iter()
                 .filter(|neighbor| !input.contains(neighbor))
         })
         .count();
 
     println!("Part 1: {part1}");
+
+    let exterior = exterior(&input);
+    let part2 = input
+        .iter()
+        .flat_map(|lava_bit| {
+            lava_bit
+                .neighbors()
+                .into_iter()
+                .filter(|neighbor| exterior.contains(neighbor))
+        })
+        .count();
+
+    println!("Part 2: {part2}");
 }
