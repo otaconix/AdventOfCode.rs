1c65502330855c79131a5325bf633635154d63f1
Stefan Zwanenburg <SCP.Zwanenburg@belastingdienst.nl>
2023-11-30 19:05:24 +0100

    2018-02: Parts 1 & 2

---
+++ years/2018/02/src/main.rs
@@ -0,0 +1,69 @@
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+    ops::ControlFlow,
+};
+
+fn main() {
+    let input = io::stdin()
+        .lines()
+        .map(|result| result.expect("I/O error"))
+        .collect::<Vec<_>>();
+
+    let letter_counts = input
+        .iter()
+        .map(|line| {
+            line.chars()
+                .fold(HashMap::new(), |mut counts, char| {
+                    if let Some(char_count) = counts.get_mut(&char) {
+                        *char_count += 1;
+                    } else {
+                        counts.insert(char, 1);
+                    }
+
+                    counts
+                })
+                .into_values()
+                .collect::<HashSet<_>>()
+        })
+        .collect::<Vec<_>>();
+    let twos = letter_counts
+        .iter()
+        .filter(|counts| counts.contains(&2))
+        .count();
+    let threes = letter_counts
+        .iter()
+        .filter(|counts| counts.contains(&3))
+        .count();
+    let part_1 = twos * threes;
+
+    println!("Part 1: {part_1:#?}");
+
+    let correct_boxes = input.iter().enumerate().try_fold((), |_, (index, box_a)| {
+        if let Some(box_b) = input.iter().skip(index + 1).find(|box_b| {
+            box_a
+                .chars()
+                .zip(box_b.chars())
+                .filter(|(a, b)| a != b)
+                .count()
+                == 1
+        }) {
+            ControlFlow::Break((box_a, box_b))
+        } else {
+            ControlFlow::Continue(())
+        }
+    });
+
+    let part_2 = if let ControlFlow::Break((box_a, box_b)) = correct_boxes {
+        box_a
+            .chars()
+            .zip(box_b.chars())
+            .filter(|(a, b)| a == b)
+            .map(|(a, _)| a)
+            .collect::<String>()
+    } else {
+        panic!("No correct boxes found!")
+    };
+
+    println!("Part 2: {part_2}");
+}
