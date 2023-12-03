b67ab5b4695b98a20935c6e8bd18f01d1fb75c36
Stefan Zwanenburg <stefan@zwanenburg.info>
2023-12-03 12:09:10 +0100

    2023-03: Part 2
    Some refactoring to be done, but it works!

--- years/2023/03/src/main.rs
+++ years/2023/03/src/main.rs
@@ -82,4 +82,60 @@ fn main() {
         .sum::<u32>();
 
     println!("Part 1: {part_1}");
+
+    let potential_gears = grid
+        .coordinates()
+        .filter(|(x, y)| {
+            let char = grid.get(*x, *y).unwrap();
+
+            char == &'*'
+        })
+        .collect::<Vec<_>>();
+
+    let gear_adjacent_number_coords = grid
+        .coordinates()
+        .filter(|(x, y)| {
+            let char = grid.get(*x, *y).unwrap();
+
+            char.is_ascii_digit()
+                && potential_gears
+                    .iter()
+                    .any(|(gx, gy)| x.abs_diff(*gx) <= 1 && y.abs_diff(*gy) <= 1)
+        })
+        .map(|coord| get_number_range(&grid, coord))
+        .collect::<HashSet<_>>();
+
+    let part_2 = potential_gears
+        .iter()
+        .filter_map(|(gx, gy)| {
+            let adjacent_number_coords = gear_adjacent_number_coords
+                .iter()
+                .filter(|coords| {
+                    coords
+                        .iter()
+                        .any(|(x, y)| x.abs_diff(*gx) <= 1 && y.abs_diff(*gy) <= 1)
+                })
+                .collect::<Vec<_>>();
+
+            if adjacent_number_coords.len() == 2 {
+                Some(
+                    adjacent_number_coords
+                        .iter()
+                        .map(|coords| {
+                            coords
+                                .iter()
+                                .map(|(x, y)| grid.get(*x, *y).unwrap())
+                                .collect::<String>()
+                                .parse::<u32>()
+                                .unwrap()
+                        })
+                        .product::<u32>(),
+                )
+            } else {
+                None
+            }
+        })
+        .sum::<u32>();
+
+    println!("Part 2: {part_2}");
 }
