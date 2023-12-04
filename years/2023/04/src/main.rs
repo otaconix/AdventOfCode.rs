05fff5e472a0fd461d1164a4d64f958251d809be
Stefan Zwanenburg <stefan@zwanenburg.info>
2023-12-04 09:54:29 +0100

    2023-04: Part 2 + some refactoring

--- years/2023/04/src/main.rs
+++ years/2023/04/src/main.rs
@@ -1,9 +1,9 @@
 use pom::utf8::*;
-use std::{io, str::FromStr};
+use std::io;
+use std::str::FromStr;
 
 #[derive(Debug)]
 struct ScratchCard {
-    id: u8,
     winning_numbers: Vec<u8>,
     scratched_numbers: Vec<u8>,
 }
@@ -21,15 +21,23 @@ impl FromStr for ScratchCard {
         };
         let id = seq("Card ") * call(number) - seq(": ");
         let number_list = || list(call(number), sym(' '));
-        let scratch_card = (id + (call(number_list) - seq(" | ")) + call(number_list)).map(
-            |((id, winning_numbers), scratched_numbers)| ScratchCard {
-                id,
+        let scratch_card = (id * (call(number_list) - seq(" | ")) + call(number_list)).map(
+            |(winning_numbers, scratched_numbers)| ScratchCard {
                 winning_numbers,
                 scratched_numbers,
             },
         );
 
         scratch_card.parse(s.as_bytes())
+    }
+}
+
+impl ScratchCard {
+    fn winning_numbers_count(&self) -> usize {
+        self.scratched_numbers
+            .iter()
+            .filter(|scratched_number| self.winning_numbers.contains(scratched_number))
+            .count()
     }
 }
 
@@ -42,18 +50,29 @@ fn main() {
 
     let part_1 = input
         .iter()
-        .map(|scratch_card| {
-            match scratch_card
-                .scratched_numbers
-                .iter()
-                .filter(|scratched_number| scratch_card.winning_numbers.contains(scratched_number))
-                .count()
-            {
-                0 => 0,
-                count => 2u32.pow(count as u32 - 1),
-            }
+        .map(|scratch_card| match scratch_card.winning_numbers_count() {
+            0 => 0,
+            count => 2u32.pow(count as u32 - 1),
         })
         .sum::<u32>();
 
     println!("Part 1: {part_1}");
+
+    let mut card_counts = std::iter::repeat(1).take(input.len()).collect::<Vec<_>>();
+
+    for index in 0..input.len() {
+        if card_counts[index] == 0 {
+            break;
+        }
+
+        let winners = input[index].winning_numbers_count();
+
+        for to_add in ((index + 1)..).take(winners) {
+            card_counts[to_add] += card_counts[index];
+        }
+    }
+
+    let part_2 = card_counts.iter().sum::<usize>();
+
+    println!("Part 2: {part_2}");
 }
