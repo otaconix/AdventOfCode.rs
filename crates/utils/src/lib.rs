fddfa3c83bbc9e3dcfafb13904f40ff832d7275a
Stefan Zwanenburg <SCP.Zwanenburg@belastingdienst.nl>
2023-12-13 14:02:16 +0100

    aoc-utils: Add PartitionEnumerated
    It allows to partition an iterator into a tuple of two arbitrary
    collections by using both the index and the value of the items in the
    predicate function.

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -6,3 +6,28 @@ where
     /// Get all variants of the enum this trait is implemented for.
     fn variants() -> [Self; N];
 }
+
+pub trait PartitionEnumerated<T> {
+    fn partition_enumerated<O: Default + Extend<T>, F: Fn(usize, &T) -> bool>(self, f: F)
+        -> (O, O);
+}
+
+impl<T, I: Iterator<Item = T> + Sized> PartitionEnumerated<T> for I {
+    fn partition_enumerated<O: Default + Extend<T>, F: Fn(usize, &T) -> bool>(
+        self,
+        f: F,
+    ) -> (O, O) {
+        let mut left = O::default();
+        let mut right = O::default();
+
+        for (i, t) in self.enumerate() {
+            if f(i, &t) {
+                left.extend(Some(t));
+            } else {
+                right.extend(Some(t));
+            }
+        }
+
+        (left, right)
+    }
+}
