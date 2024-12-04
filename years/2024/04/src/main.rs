362d2802841c7b79969b9d6b75968a389fcb04af
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-04 21:25:29 +0100

    2024-04 Done
    Not the prettiest. I somehow had trouble finding a way to get all the
    diagonals... Let's just say I'm foggy.

--- years/2024/04/src/main.rs
+++ years/2024/04/src/main.rs
@@ -1,23 +1,99 @@
 use std::io;
 
 use aoc_timing::trace::log_run;
+use grid::Grid;
 
-type Input = Vec<String>;
+type Input = Grid<char>;
 
 fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
-    input.map(|line| {
-        let line = line.as_ref();
+    Grid::new(
+        input
+            .map(|line| {
+                let line = line.as_ref();
 
-        todo!()
-    })
+                line.chars().collect()
+            })
+            .collect(),
+    )
+    .expect("Couldn't create grid")
 }
 
+const XMAS: &str = "XMAS";
+const SAMX: &str = "SAMX";
+
 fn part_1(input: &Input) -> usize {
-    todo!()
+    let mut count = 0;
+
+    for row in 0..input.height() {
+        let row = input.row(row).collect::<String>();
+        for x in 0..=row.len() - 4 {
+            let maybe_xmas = &row[x..x + 4];
+            if maybe_xmas == XMAS || maybe_xmas == SAMX {
+                count += 1;
+            }
+        }
+    }
+
+    for column in 0..input.width() {
+        let column = input.column(column).collect::<String>();
+        for x in 0..=column.len() - 4 {
+            let maybe_xmas = &column[x..x + 4];
+            if maybe_xmas == XMAS || maybe_xmas == SAMX {
+                count += 1;
+            }
+        }
+    }
+
+    // Down & to the right
+    for y in 0..input.height() - 3 {
+        for x in 0..input.width() - 3 {
+            let maybe_xmas: String = [(x, y), (x + 1, y + 1), (x + 2, y + 2), (x + 3, y + 3)]
+                .into_iter()
+                .map(|(x, y)| input.get(x, y).unwrap())
+                .collect();
+
+            if maybe_xmas == XMAS || maybe_xmas == SAMX {
+                count += 1;
+            }
+        }
+    }
+
+    // Down & to the left
+    for y in 0..input.height() - 3 {
+        for x in (3..input.width()).rev() {
+            let maybe_xmas: String = [(x, y), (x - 1, y + 1), (x - 2, y + 2), (x - 3, y + 3)]
+                .into_iter()
+                .map(|(x, y)| input.get(x, y).unwrap())
+                .collect();
+
+            if maybe_xmas == XMAS || maybe_xmas == SAMX {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 fn part_2(input: &Input) -> usize {
-    todo!()
+    input
+        .coordinates()
+        .filter(|(x, y)| {
+            (1..input.width() - 1).contains(x)
+                && (1..input.height() - 1).contains(y)
+                && input.get(*x, *y).unwrap() == &'A'
+        })
+        .filter(|(x, y)| {
+            ((input.get(*x - 1, *y - 1).unwrap() == &'M'
+                && input.get(x + 1, y + 1).unwrap() == &'S')
+                || (input.get(*x - 1, *y - 1).unwrap() == &'S'
+                    && input.get(x + 1, y + 1).unwrap() == &'M'))
+                && ((input.get(*x - 1, *y + 1).unwrap() == &'M'
+                    && input.get(x + 1, y - 1).unwrap() == &'S')
+                    || (input.get(*x - 1, *y + 1).unwrap() == &'S'
+                        && input.get(x + 1, y - 1).unwrap() == &'M'))
+        })
+        .count()
 }
 
 fn main() {
@@ -47,7 +123,7 @@ mod tests {
         let input = parse(INPUT.lines());
         let result = part_1(&input);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 18);
     }
 
     #[test]
@@ -55,6 +131,6 @@ mod tests {
         let input = parse(INPUT.lines());
         let result = part_2(&input);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 9);
     }
 }
