a6674b106a6b3daca3037fb5bec54cc151b6791a
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-04 08:57:38 +0100

    Added template for 2024-04

---
+++ years/2024/04/src/main.rs
@@ -0,0 +1,60 @@
+use std::io;
+
+use aoc_timing::trace::log_run;
+
+type Input = Vec<String>;
+
+fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
+    input.map(|line| {
+        let line = line.as_ref();
+
+        todo!()
+    })
+}
+
+fn part_1(input: &Input) -> usize {
+    todo!()
+}
+
+fn part_2(input: &Input) -> usize {
+    todo!()
+}
+
+fn main() {
+    env_logger::init();
+
+    log_run("Full run", || {
+        let input = log_run("Parsing", || {
+            parse(io::stdin().lines().map(|result| result.expect("I/O error")))
+        });
+
+        let part_1 = log_run("Part 1", || part_1(&input));
+        println!("Part 1: {part_1}");
+
+        let part_2 = log_run("Part 2", || part_2(&input));
+        println!("Part 2: {part_2}");
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("test-input");
+
+    #[test]
+    fn test_part_1() {
+        let input = parse(INPUT.lines());
+        let result = part_1(&input);
+
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = parse(INPUT.lines());
+        let result = part_2(&input);
+
+        assert_eq!(result, 0);
+    }
+}
