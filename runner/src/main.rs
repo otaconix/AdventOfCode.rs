0b5268e2b512ad86a5727a270b71331a59ba3e9d
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-06 10:55:25 +0100

    runner: use clap to read AOC_SESSION & AOC_CONTACT_INFO environment variables

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,7 +3,6 @@ use directories::ProjectDirs;
 use dotenvy::dotenv;
 use reqwest::blocking::ClientBuilder;
 use reqwest::header::COOKIE;
-use std::env;
 use std::fs::{create_dir_all, write, File};
 use std::os::unix::process::CommandExt;
 use std::process::{exit, Command, Stdio};
@@ -14,15 +13,16 @@ struct Opt {
     day: u8,
     command: String,
     args: Vec<String>,
+    #[arg(long = "contact-info", env = "AOC_CONTACT_INFO")]
+    contact_info: String,
+    #[arg(long = "session", env = "AOC_SESSION")]
+    session: String,
 }
-
-const CONTACT_INFO: &str = "AOC_CONTACT_INFO";
-const SESSION: &str = "AOC_SESSION";
 
 fn main() {
     dotenv().ok();
     let opt = Opt::parse();
-    let input_file = get_input_file(opt.year, opt.day).unwrap();
+    let input_file = get_input_file(&opt).unwrap();
 
     let exec_error = Command::new(opt.command)
         .args(opt.args)
@@ -33,9 +33,7 @@ fn main() {
     exit(1);
 }
 
-fn get_input_file(year: u16, day: u8) -> Result<File, String> {
-    let contact_info = env::var(CONTACT_INFO).unwrap();
-    let session = env::var(SESSION).unwrap();
+fn get_input_file(opt: &Opt) -> Result<File, String> {
     let cache_dir = &ProjectDirs::from(
         "nl",
         "svanur",
@@ -43,8 +41,8 @@ fn get_input_file(year: u16, day: u8) -> Result<File, String> {
     )
     .ok_or("Couldn't determine cache dir")?
     .cache_dir()
-    .join(year.to_string());
-    let cache_file = cache_dir.join(format!("{day:02}"));
+    .join(opt.year.to_string());
+    let cache_file = cache_dir.join(format!("{:02}", opt.day));
 
     create_dir_all(cache_dir).map_err(|e| {
         format!(
@@ -56,12 +54,15 @@ fn get_input_file(year: u16, day: u8) -> Result<File, String> {
     if !cache_file.exists() {
         eprintln!("Puzzle input not cached, fetching from adventofcode.com");
         let client = ClientBuilder::new()
-            .user_agent(contact_info)
+            .user_agent(&opt.contact_info)
             .build()
             .map_err(|e| format!("Couldn't create HTTP client: {e}"))?;
         let response = client
-            .get(format!("https://adventofcode.com/{year}/day/{day}/input"))
-            .header(COOKIE, format!("session={session}"))
+            .get(format!(
+                "https://adventofcode.com/{}/day/{}/input",
+                opt.year, opt.day
+            ))
+            .header(COOKIE, format!("session={}", opt.session))
             .send()
             .map_err(|e| format!("{}", e))?
             .error_for_status()
