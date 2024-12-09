76bcd3a53f38eb7ebb22930836fe295927a6ddfa
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-09 21:05:59 +0100

    2024-09: Rewrote part 1 to also use block sequences

--- years/2024/09/src/main.rs
+++ years/2024/09/src/main.rs
@@ -2,57 +2,8 @@ use std::io;
 
 use aoc_timing::debug::log_run;
 
-type Input = Vec<usize>;
+type Input = Vec<BlockSequence>;
 type Output = usize;
-
-fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
-    input
-        .map(|line| {
-            let line = line.as_ref();
-
-            line.chars()
-                .map(|c| format!("{c}").parse().unwrap())
-                .collect()
-        })
-        .next()
-        .unwrap()
-}
-
-fn part_1(input: &Input) -> Output {
-    let mut start_index = 0;
-    // Make sure that, if the last element in the input is empty space, we don't use it as though
-    // it were a file.
-    let mut end_index = (input.len() - 1) & !1;
-    let mut result = 0;
-    let mut input = input.clone();
-
-    for pos in 0.. {
-        if start_index > end_index || start_index >= input.len() {
-            break;
-        } else if start_index % 2 == 1 {
-            // Take from the end
-            result += pos * (end_index / 2);
-            input[end_index] -= 1;
-        } else {
-            // Take from the start
-            result += pos * (start_index / 2);
-        }
-
-        input[start_index] -= 1;
-
-        // Skip over zero-sized or exhausted blocks
-        while start_index < input.len() && input[start_index] == 0 {
-            start_index += 1;
-        }
-
-        // Skip over zero-sized or exhausted blocks
-        while end_index > 0 && input[end_index] == 0 {
-            end_index -= 2;
-        }
-    }
-
-    result
-}
 
 #[derive(Debug, Clone, Copy)]
 struct File {
@@ -66,24 +17,89 @@ enum BlockSequence {
     File(File),
 }
 
-fn part_2(input: &Input) -> Output {
-    let mut sequences: Vec<_> = input
-        .iter()
-        .copied()
-        .enumerate()
-        .map(|(index, block_count)| {
-            if index % 2 == 0 {
-                BlockSequence::File(File {
-                    index: index / 2,
-                    block_count,
-                })
-            } else {
-                BlockSequence::Empty(block_count)
-            }
-        })
-        .collect();
+fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
+    input
+        .map(|line| {
+            let line = line.as_ref();
 
-    for index in (1..sequences.len()).rev() {
+            line.chars()
+                .enumerate()
+                .map(|(index, c)| {
+                    let block_count = format!("{c}").parse::<usize>().unwrap();
+
+                    if index % 2 == 0 {
+                        BlockSequence::File(File {
+                            block_count,
+                            index: index / 2,
+                        })
+                    } else {
+                        BlockSequence::Empty(block_count)
+                    }
+                })
+                .collect()
+        })
+        .next()
+        .unwrap()
+}
+
+fn part_1(input: &Input) -> Output {
+    // Make sure that, if the last element in the input is empty space, we don't use it as though
+    // it were a file.
+    let mut end_index = (input.len() - 1) & !1;
+    let mut result = 0;
+    let mut block_position = 0;
+    let mut input = input.clone();
+
+    for start_index in 0..input.len() {
+        if start_index > end_index {
+            break;
+        }
+
+        match input[start_index] {
+            BlockSequence::Empty(mut empty_block_count) => {
+                while start_index < end_index && empty_block_count > 0 {
+                    if let BlockSequence::File(ref mut file) = input[end_index] {
+                        let moved_block_count = if empty_block_count > file.block_count {
+                            file.block_count
+                        } else {
+                            empty_block_count
+                        };
+
+                        file.block_count -= moved_block_count;
+                        empty_block_count -= moved_block_count;
+
+                        result +=
+                            file.index * (block_position..).take(moved_block_count).sum::<usize>();
+                        block_position += moved_block_count;
+
+                        while end_index > 0
+                            && match input[end_index] {
+                                BlockSequence::File(file) if file.block_count == 0 => true,
+                                BlockSequence::Empty(_) => true,
+                                _ => false,
+                            }
+                        {
+                            end_index -= 1;
+                        }
+                    }
+                }
+            }
+            BlockSequence::File(ref mut file) => {
+                result += file.index * (block_position..).take(file.block_count).sum::<usize>();
+                block_position += file.block_count;
+                file.block_count = 0;
+            }
+        }
+    }
+
+    result
+}
+
+fn part_2(input: &Input) -> Output {
+    let mut sequences: Vec<_> = input.clone();
+    let end_index = (sequences.len() - 1) & !1;
+
+    for index in (1..=end_index).rev() {
         let file = if let BlockSequence::File(file) = sequences[index] {
             file
         } else {
