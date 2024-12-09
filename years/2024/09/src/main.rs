eedff3977cca64deee3aa777a096aca98482239b
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-09 19:40:11 +0100

    2024-09 Part 2 done (suboptimal)
    I think I can do part 2 much the same way I did part 1: in one pass.
    Right now, the implementation goes over the entire list as many times as
    there are files, which takes a good while with the non-test input, since
    that one's quite large.

--- years/2024/09/src/main.rs
+++ years/2024/09/src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 
-use aoc_timing::trace::log_run;
+use aoc_timing::debug::log_run;
 
-type Input = Vec<u32>;
+type Input = Vec<usize>;
 type Output = usize;
 
 fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
@@ -20,7 +20,9 @@ fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
 
 fn part_1(input: &Input) -> Output {
     let mut start_index = 0;
-    let mut end_index = input.len() - 1;
+    // Make sure that, if the last element in the input is empty space, we don't use it as though
+    // it were a file.
+    let mut end_index = (input.len() - 1) & !1;
     let mut result = 0;
     let mut input = input.clone();
 
@@ -29,12 +31,10 @@ fn part_1(input: &Input) -> Output {
             break;
         } else if start_index % 2 == 1 {
             // Take from the end
-            // println!("end:   {pos} * {}", end_index / 2);
             result += pos * (end_index / 2);
             input[end_index] -= 1;
         } else {
             // Take from the start
-            // println!("start: {pos} * {}", start_index / 2);
             result += pos * (start_index / 2);
         }
 
@@ -54,8 +54,73 @@ fn part_1(input: &Input) -> Output {
     result
 }
 
+#[derive(Debug, Clone, Copy)]
+struct File {
+    index: usize,
+    block_count: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum BlockSequence {
+    Empty(usize),
+    File(File),
+}
+
 fn part_2(input: &Input) -> Output {
-    todo!()
+    let mut sequences: Vec<_> = input
+        .iter()
+        .copied()
+        .enumerate()
+        .map(|(index, block_count)| {
+            if index % 2 == 0 {
+                BlockSequence::File(File {
+                    index: index / 2,
+                    block_count,
+                })
+            } else {
+                BlockSequence::Empty(block_count)
+            }
+        })
+        .collect();
+
+    for index in (1..sequences.len()).rev() {
+        let file = if let BlockSequence::File(file) = sequences[index] {
+            file
+        } else {
+            continue;
+        };
+
+        for empty_index in 0..index {
+            match sequences[empty_index] {
+                BlockSequence::Empty(ref mut empty_block_count)
+                    if *empty_block_count >= file.block_count =>
+                {
+                    *empty_block_count -= file.block_count;
+
+                    sequences[index] = BlockSequence::Empty(file.block_count);
+                    sequences.insert(empty_index, BlockSequence::File(file));
+
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    let mut result = 0;
+    let mut block_position = 0;
+    for sequence in sequences {
+        match sequence {
+            BlockSequence::Empty(block_count) => block_position += block_count,
+            BlockSequence::File(file) => {
+                result += file.index * (block_position..).take(file.block_count).sum::<usize>();
+
+                block_position += file.block_count;
+            }
+        }
+    }
+
+    result
 }
 
 fn main() {
@@ -93,6 +158,6 @@ mod tests {
         let input = parse(INPUT.lines());
         let result = part_2(&input);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 2858);
     }
 }
