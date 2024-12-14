22f66dbc7bffa1317bd869d321fea5dcab3c47a0
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-14 22:38:45 +0100

    2024-14 Part 1 done
    Now how the heck am I going to automate part 2? I have to look for a
    picture of a christmas tree...

---
+++ years/2024/14/src/main.rs
@@ -0,0 +1,102 @@
+use std::io;
+
+use aoc_timing::trace::log_run;
+
+const WIDTH: isize = 101;
+const HEIGHT: isize = 103;
+
+struct Robot {
+    x: isize,
+    y: isize,
+    vx: isize,
+    vy: isize,
+}
+
+impl Robot {
+    fn move_for(&self, duration: isize, width: isize, height: isize) -> (isize, isize) {
+        (
+            (self.x + self.vx * duration).rem_euclid(width),
+            (self.y + self.vy * duration).rem_euclid(height),
+        )
+    }
+}
+
+type Input = Vec<Robot>;
+type Output = usize;
+
+fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
+    input
+        .map(|line| {
+            let line = line.as_ref();
+
+            let (position, velocity) = line.split_once(' ').unwrap();
+            let (x, y) = position.split_once('=').unwrap().1.split_once(',').unwrap();
+            let (vx, vy) = velocity.split_once('=').unwrap().1.split_once(',').unwrap();
+
+            Robot {
+                x: x.parse().unwrap(),
+                y: y.parse().unwrap(),
+                vx: vx.parse().unwrap(),
+                vy: vy.parse().unwrap(),
+            }
+        })
+        .collect()
+}
+
+fn part_1(input: &Input, width: isize, height: isize) -> Output {
+    let (top, bottom): (Vec<_>, Vec<_>) = input
+        .iter()
+        .map(|robot| robot.move_for(100, width, height))
+        .filter(|(x, y)| *x != width / 2 && *y != height / 2)
+        .partition(|(_, y)| *y < height / 2);
+
+    let (top_left, top_right): (Vec<_>, Vec<_>) =
+        top.into_iter().partition(|(x, _)| *x < width / 2);
+    let (bottom_left, bottom_right): (Vec<_>, Vec<_>) =
+        bottom.into_iter().partition(|(x, _)| *x < width / 2);
+
+    top_left.len() * top_right.len() * bottom_left.len() * bottom_right.len()
+}
+
+fn part_2(input: &Input) -> Output {
+    todo!()
+}
+
+fn main() {
+    env_logger::init();
+
+    log_run("Full run", || {
+        let input = log_run("Parsing", || {
+            parse(io::stdin().lines().map(|result| result.expect("I/O error")))
+        });
+
+        let part_1 = log_run("Part 1", || part_1(&input, WIDTH, HEIGHT));
+        println!("Part 1: {part_1}");
+
+        let part_2 = log_run("Part 2", || part_2(&input));
+        println!("Part 2: {part_2}");
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("test-input");
+
+    #[test]
+    fn test_part_1() {
+        let input = parse(INPUT.lines());
+        let result = part_1(&input, 11, 7);
+
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = parse(INPUT.lines());
+        let result = part_2(&input);
+
+        assert_eq!(result, 0);
+    }
+}
