32f4d79e03359a5145729c8ac8979538628cc366
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-15 23:56:55 +0100

    2018-01 Rewritten with template

--- years/2018/01/src/main.rs
+++ years/2018/01/src/main.rs
@@ -1,41 +1,86 @@
-use aoc_timing::trace::log_run;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::io;
 use std::ops::ControlFlow;
+
+use aoc_timing::trace::log_run;
+
+type Input = Vec<isize>;
+type Output = isize;
+
+fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
+    input
+        .map(|line| {
+            let line = line.as_ref();
+
+            line.parse().expect("Couldn't parse as number")
+        })
+        .collect()
+}
+
+fn part_1(input: &Input) -> Output {
+    input.iter().sum()
+}
+
+fn part_2(input: &Input) -> Output {
+    if let ControlFlow::Break(frequency) = input
+        .iter()
+        .cycle()
+        .scan(0, |frequency, change| {
+            Some({
+                *frequency += change;
+                *frequency
+            })
+        })
+        .try_fold(BTreeSet::new(), |mut seen, frequency| {
+            if seen.contains(&frequency) {
+                ControlFlow::Break(frequency)
+            } else {
+                seen.insert(frequency);
+                ControlFlow::Continue(seen)
+            }
+        })
+    {
+        frequency
+    } else {
+        panic!()
+    }
+}
 
 fn main() {
     env_logger::init();
 
-    let input = io::stdin()
-        .lines()
-        .map(|result| result.expect("I/O error"))
-        .map(|line| line.parse::<i32>().expect("Couldn't parse number"))
-        .collect::<Vec<_>>();
+    log_run("Full run", || {
+        let input = log_run("Parsing", || {
+            parse(io::stdin().lines().map(|result| result.expect("I/O error")))
+        });
 
-    let part_1 = log_run("Part 1", || input.iter().sum::<i32>());
-    println!("Part 1: {part_1}");
+        let part_1 = log_run("Part 1", || part_1(&input));
+        println!("Part 1: {part_1}");
 
-    let part_2 = log_run("Part 2", || {
-        match input
-            .iter()
-            .cycle()
-            .scan(0, |acc, change| {
-                *acc += change;
-
-                Some(*acc)
-            })
-            .try_fold(HashSet::new(), |mut already_seen, next| {
-                if !already_seen.contains(&next) {
-                    already_seen.insert(next);
-                    ControlFlow::Continue(already_seen)
-                } else {
-                    ControlFlow::Break(next)
-                }
-            }) {
-            ControlFlow::Break(result) => result,
-            _ => panic!("Couldn't find a result!"),
-        }
+        let part_2 = log_run("Part 2", || part_2(&input));
+        println!("Part 2: {part_2}");
     });
+}
 
-    println!("Part 2: {part_2:#?}");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("test-input");
+
+    #[test]
+    fn test_part_1() {
+        let input = parse(INPUT.lines());
+        let result = part_1(&input);
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = parse(INPUT.lines());
+        let result = part_2(&input);
+
+        assert_eq!(result, 2);
+    }
 }
