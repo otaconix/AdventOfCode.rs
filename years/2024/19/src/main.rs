f8f87f9f639d9626469f92b08599005714d2f3f5
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-19 11:01:14 +0100

    2024-19: Part 1 done
    I was experimenting with using a Trie for the towels. Maybe I'll try it
    again when I'm done.

---
+++ years/2024/19/src/main.rs
@@ -0,0 +1,116 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io;
+
+use aoc_timing::trace::log_run;
+
+struct Input {
+    towels: HashSet<String>,
+    designs: Vec<String>,
+}
+type Output1 = usize;
+type Output2 = usize;
+
+fn parse<'a, S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
+    enum State {
+        Towels(HashSet<String>),
+        Designs(HashSet<String>, Vec<String>),
+    }
+    let end_state = input.fold(State::Towels(HashSet::new()), |state, line| {
+        let line = line.as_ref();
+
+        match state {
+            State::Towels(mut towels) => {
+                if line.is_empty() {
+                    State::Designs(towels, vec![])
+                } else {
+                    towels.extend(line.split(", ").map(|towel| towel.to_string()));
+                    State::Towels(towels)
+                }
+            }
+            State::Designs(towels, mut designs) => {
+                designs.push(line.to_string());
+                State::Designs(towels, designs)
+            }
+        }
+    });
+
+    if let State::Designs(towels, designs) = end_state {
+        Input { towels, designs }
+    } else {
+        panic!("Didn't reach the designs part of the input?");
+    }
+}
+
+fn design_is_possible<'a>(
+    design: &'a str,
+    towels: &HashSet<String>,
+    cache: &mut HashMap<&'a str, bool>,
+) -> bool {
+    if let Some(result) = cache.get(design) {
+        *result
+    } else if design.is_empty() {
+        true
+    } else {
+        let result = towels
+            .iter()
+            .filter(|t| design.starts_with(*t))
+            .any(|t| design_is_possible(&design[t.len()..], towels, cache));
+        cache.insert(design, result);
+
+        result
+    }
+}
+
+fn part_1(input: &Input) -> Output1 {
+    let mut cache = Default::default();
+
+    input
+        .designs
+        .iter()
+        .filter(|design| design_is_possible(design, &input.towels, &mut cache))
+        .count()
+}
+
+fn part_2(input: &Input) -> Output2 {
+    todo!()
+}
+
+fn main() {
+    env_logger::init();
+
+    log_run("Full run", || {
+        let input = log_run("Parsing", || {
+            parse(io::stdin().lines().map(|result| result.expect("I/O error")))
+        });
+
+        let part_1 = log_run("Part 1", || part_1(&input));
+        println!("Part 1: {part_1}");
+
+        let part_2 = log_run("Part 2", || part_2(&input));
+        println!("Part 2: {part_2}");
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("test-input");
+
+    #[test]
+    fn test_part_1() {
+        let input = parse(INPUT.lines());
+        let result = part_1(&input);
+
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = parse(INPUT.lines());
+        let result = part_2(&input);
+
+        assert_eq!(result, 0);
+    }
+}
