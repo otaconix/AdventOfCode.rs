5dd4e0816e6b06adcfdc44260608a7126137ab24
Stefan Zwanenburg <stefan@zwanenburg.info>
2024-12-30 17:18:22 +0100

    2023-20 Part 2 also done

--- years/2023/20/src/main.rs
+++ years/2023/20/src/main.rs
@@ -1,109 +1,16 @@
-use std::cell::RefCell;
-use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
-use std::fmt::{Debug, Display, Formatter, Write};
+use std::cell::OnceCell;
+use std::collections::{HashMap, VecDeque};
+use std::fmt::{Display, Formatter};
 use std::io;
-use std::rc::Rc;
 
 use aoc_timing::trace::log_run;
+use fxhash::FxHashMap;
 use log::debug;
 
-type Input = HashMap<String, (Module, Vec<String>)>;
-
-struct DependencyTree<'a> {
-    name: &'a str,
-    root: &'a Module,
-    dependencies: Vec<Rc<RefCell<DependencyTree<'a>>>>,
-}
-
-impl Display for DependencyTree<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        fn inner<'a>(
-            tree: &DependencyTree<'a>,
-            depth: usize,
-            already_seen: &mut HashSet<&'a str>,
-            f: &mut Formatter<'_>,
-        ) -> std::fmt::Result {
-            let width = depth * 2;
-            let prefix = "";
-
-            f.write_fmt(format_args!("{:>width$}Name: {}\n", prefix, tree.name,))?;
-            f.write_fmt(format_args!("{:>width$}Module: {}\n", "", tree.root))?;
-
-            if already_seen.contains(tree.name) {
-                f.write_fmt(format_args!("{:>width$}Dependencies already seen", ""))?;
-            } else if tree.dependencies.is_empty() {
-                f.write_fmt(format_args!("{:>width$}No dependencies", ""))?;
-            } else {
-                already_seen.insert(tree.name);
-                f.write_fmt(format_args!("{:>width$}Dependencies:", ""))?;
-                for dependency in &tree.dependencies {
-                    f.write_char('\n')?;
-                    inner(&dependency.borrow(), depth + 1, already_seen, f)?;
-                }
-            }
-
-            Ok(())
-        }
-
-        let mut already_seen = HashSet::new();
-        inner(self, 0, &mut already_seen, f)?;
-
-        Ok(())
-    }
-}
-
-const RX_NAME: &str = "rx";
-
-fn construct_dependency_tree(input: &Input) -> DependencyTree<'_> {
-    fn inner<'a>(
-        input: &'a Input,
-        name: &'a str,
-        already_seen: &mut HashMap<String, Rc<RefCell<DependencyTree<'a>>>>,
-    ) -> Rc<RefCell<DependencyTree<'a>>> {
-        if let Some(already_seen) = already_seen.get(name) {
-            already_seen.clone()
-        } else {
-            let (root, _) = &input[name];
-            let result = Rc::new(RefCell::new(DependencyTree {
-                name,
-                root,
-                dependencies: vec![],
-            }));
-
-            already_seen.insert(name.to_string(), result.clone());
-            result.borrow_mut().dependencies.extend(
-                input
-                    .iter()
-                    .filter(|(_, (_, destinations))| destinations.contains(&name.to_string()))
-                    .map(|(name, _)| inner(input, name, already_seen)),
-            );
-
-            if matches!(root, Module::Broadcast) {
-                result
-                    .borrow_mut()
-                    .dependencies
-                    .push(Rc::new(RefCell::new(DependencyTree {
-                        name: "button",
-                        root: &Module::Button,
-                        dependencies: vec![],
-                    })));
-            }
-
-            result
-        }
-    }
-
-    let mut already_seen = HashMap::new();
-
-    DependencyTree {
-        name: RX_NAME,
-        root: &Module::FinalDestination,
-        dependencies: input
-            .iter()
-            .filter(|(_, (_, destinations))| destinations.contains(&RX_NAME.to_string()))
-            .map(|(name, _)| inner(input, name, &mut already_seen))
-            .collect(),
-    }
+#[derive(Debug, Clone)]
+struct Input {
+    links: HashMap<String, Vec<String>>,
+    modules: HashMap<String, Module>,
 }
 
 #[derive(Hash, Clone, Copy, Debug, PartialEq, Eq)]
@@ -112,24 +19,31 @@ enum Pulse {
     High,
 }
 
-#[derive(Clone, Hash, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum Module {
-    Button,
-    FinalDestination,
     Broadcast,
     FlipFlop {
         on: bool,
     },
     Conjunction {
-        last_inputs: BTreeMap<String, Pulse>,
+        last_inputs: FxHashMap<String, Pulse>,
     },
+}
+
+impl Module {
+    #[allow(dead_code)]
+    fn symbol(&self) -> char {
+        match self {
+            Module::Broadcast => '!',
+            Module::FlipFlop { .. } => '%',
+            Module::Conjunction { .. } => '&',
+        }
+    }
 }
 
 impl Display for Module {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_str(match self {
-            Module::Button => "button",
-            Module::FinalDestination => "final destination",
             Module::Broadcast => "broadcast",
             Module::FlipFlop { on: _ } => "flip-flop",
             Module::Conjunction { last_inputs: _ } => "conjunction",
@@ -168,7 +82,6 @@ impl Module {
 
                 Some(output_pulse)
             }
-            _ => None,
         }
     }
 
@@ -181,8 +94,12 @@ impl Module {
 }
 
 fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
-    let mut input: Input = input
-        .map(|line| {
+    let (mut modules, links) = input.fold(
+        (
+            HashMap::<String, Module>::default(),
+            HashMap::<String, Vec<String>>::default(),
+        ),
+        |(mut modules, mut links), line| {
             let (name, destinations) = line.as_ref().split_once(" -> ").unwrap();
             let destinations = destinations
                 .split(", ")
@@ -193,62 +110,61 @@ fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
                 Some('&') => (
                     name[1..].to_string(),
                     Module::Conjunction {
-                        last_inputs: BTreeMap::new(),
+                        last_inputs: FxHashMap::default(),
                     },
                 ),
                 Some('%') => (name[1..].to_string(), Module::FlipFlop { on: false }),
                 _ => (name.to_string(), Module::Broadcast),
             };
 
-            (name, (module, destinations))
-        })
-        .collect();
+            modules.entry(name.clone()).or_insert(module);
+            links.entry(name).or_default().extend(destinations);
 
-    let source_to_destination = input
-        .iter()
-        .flat_map(|(name, (_, destinations))| {
-            destinations
-                .iter()
-                .map(|destination| (name.clone(), destination.clone()))
-        })
-        .collect::<Vec<_>>();
+            (modules, links)
+        },
+    );
 
-    for (source, destination) in source_to_destination {
-        if let Some(ref mut module) = input.get_mut(&destination) {
-            module.0.register_source(&source);
+    for (source, destinations) in &links {
+        for destination in destinations {
+            if let Some(module) = modules.get_mut(destination) {
+                module.register_source(source);
+            }
         }
     }
 
-    input
+    Input { modules, links }
 }
 
-fn push_button<S, F: Fn(S, (Pulse, &Vec<String>)) -> (S, bool)>(
+const BUTTON_DESTINATIONS: OnceCell<Vec<String>> = OnceCell::new();
+
+fn push_button<S, F: Fn(S, (Pulse, &[String])) -> S>(
     input: &mut Input,
     state: S,
     update_state: F,
 ) -> S {
+    debug!("== Pressing button ===");
     let mut pulse_queue = VecDeque::new();
+    let binding = BUTTON_DESTINATIONS;
     pulse_queue.push_back((
         "button".to_string(),
         Pulse::Low,
-        vec!["broadcaster".to_string()],
+        binding.get_or_init(|| vec!["broadcaster".to_string()]),
     ));
 
     let mut state = state;
 
     while let Some((source, pulse, destinations)) = pulse_queue.pop_front() {
-        let (new_state, do_break) = update_state(state, (pulse, &destinations));
-        state = new_state;
-        if do_break {
-            break;
-        }
+        state = update_state(state, (pulse, destinations));
 
-        pulse_queue.extend(destinations.into_iter().filter_map(|destination| {
+        pulse_queue.extend(destinations.iter().filter_map(|destination| {
             debug!("{source} -{pulse:?}-> {destination}");
-            if let Some((ref mut module, destinations)) = input.get_mut(&destination) {
-                module
-                    .process_pulse(&source, pulse)
-                    .map(|new_pulse| (destination.to_owned(), new_pulse, destinations.clone()))
+            if let Some(module) = input.modules.get_mut(destination) {
+                module.process_pulse(&source, pulse).and_then(|new_pulse| {
+                    input
+                        .links
+                        .get(destination)
+                        .map(|destinations| (destination.to_string(), new_pulse, destinations))
+                })
             } else {
                 None
             }
@@ -265,50 +181,77 @@ fn part_1(input: &Input) -> usize {
         push_button(
             &mut input,
             state,
-            |(sent_low, sent_high), (pulse, destinations)| {
-                (
-                    match pulse {
-                        Pulse::Low => (sent_low + destinations.len(), sent_high),
-                        Pulse::High => (sent_low, sent_high + destinations.len()),
-                    },
-                    false,
-                )
+            |(sent_low, sent_high), (pulse, destinations)| match pulse {
+                Pulse::Low => (sent_low + destinations.len(), sent_high),
+                Pulse::High => (sent_low, sent_high + destinations.len()),
             },
         )
     });
 
+    debug!("Sent: low={sent_low}; high={sent_high}");
     sent_low * sent_high
 }
 
+/// Ugly solution. Basically, I looked at the input, and concluded that:
+/// 1. `rx` has only a single input: a conjunction module
+/// 2. That conjunction module has a bunch of inputs, all of which are also conjunctions
+///
+/// So, let's figure out how many button presses are needed to get a _low_ pulse out of the
+/// conjunctions twice removed from `rx`, and multiply them together, to find the lowest number at
+/// which all of them output a _low_ pulse.
 fn part_2(input: &Input) -> usize {
-    let dependency_tree = construct_dependency_tree(input);
+    let mut input: Input = input.clone();
+    let rx_input = input
+        .links
+        .iter()
+        .find_map(|(module, destinations)| {
+            if destinations.contains(&"rx".to_string()) {
+                Some(module)
+            } else {
+                None
+            }
+        })
+        .expect("No 'rx' module found");
+    let rx_input_inputs = input
+        .links
+        .iter()
+        .filter_map(|(module, destinations)| {
+            if destinations.contains(rx_input) {
+                Some(module)
+            } else {
+                None
+            }
+        })
+        .cloned()
+        .collect::<Vec<_>>();
 
-    println!("{dependency_tree}");
+    use std::ops::ControlFlow::*;
 
-    0
-    //
-    // let mut input: Input = input.clone();
-    //
-    // let rx: String = String::from("rx");
-    //
-    // use ControlFlow::*;
-    // let button_presses = (1..).try_fold(false, |do_break, button_presses| {
-    //     let do_break = push_button(&mut input, do_break, |_, (pulse, destinations)| {
-    //         let result = matches!(pulse, Pulse::Low) && destinations.contains(&rx);
-    //         (result, result)
-    //     });
-    //
-    //     if do_break {
-    //         Break(button_presses)
-    //     } else {
-    //         Continue(do_break)
-    //     }
-    // });
-    //
-    // match button_presses {
-    //     Break(button_presses) => button_presses,
-    //     _ => panic!(),
-    // }
+    let input_inputs_lows = (1..).try_fold(FxHashMap::default(), |state, button_presses| {
+        let state = push_button(&mut input, state, |mut state, (pulse, destinations)| {
+            if pulse == Pulse::Low {
+                rx_input_inputs
+                    .iter()
+                    .filter(|input_inputs| destinations.contains(*input_inputs))
+                    .for_each(|input_input| {
+                        state.insert(input_input, button_presses);
+                    });
+            }
+
+            state
+        });
+
+        if state.len() == rx_input_inputs.len() {
+            Break(state)
+        } else {
+            Continue(state)
+        }
+    });
+
+    match input_inputs_lows {
+        Break(map) => map.into_values().product(),
+        _ => panic!("Broke out of loop?!"),
+    }
 }
 
 fn main() {
@@ -318,6 +261,18 @@ fn main() {
         let input = log_run("Parsing", || {
             parse(io::stdin().lines().map(|result| result.expect("I/O error")))
         });
+
+        // log_run("Writing dot for input", || {
+        //     println!("digraph {{");
+        //     for (name, module) in &input.modules {
+        //         println!("  {name} [label=\"{}{name}\"]", module.symbol());
+        //
+        //         for destination in input.links.get(name).unwrap_or(&vec![]) {
+        //             println!("  {name} -> {destination}")
+        //         }
+        //     }
+        //     println!("}}");
+        // });
 
         let part_1 = log_run("Part 1", || part_1(&input));
         println!("Part 1: {part_1}");
@@ -333,19 +288,16 @@ mod tests {
 
     const INPUT: &str = include_str!("test-input");
 
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
     #[test]
     fn test_part_1() {
+        init();
         let input = parse(INPUT.lines());
         let result = part_1(&input);
 
         assert_eq!(result, 32000000);
     }
-
-    // #[test]
-    // fn test_part_2() {
-    //     let input = parse(INPUT.lines());
-    //     let result = part_2(&input);
-    //
-    //     assert_eq!(result, 0);
-    // }
 }
