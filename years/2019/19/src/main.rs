002143b2d80f612f718f3ef6e670f16396e2fcbc
Stefan Zwanenburg <stefan+git@zwanenburg.info>
2025-11-15 19:38:48 +0100

    2019-19 Part 1 done

--- years/2019/19/src/main.rs
+++ years/2019/19/src/main.rs
@@ -1,23 +1,58 @@
+use std::collections::VecDeque;
 use std::io;
 
 use aoc_timing::trace::log_run;
+use intcode::Computer;
+use itertools::Itertools;
+use itertools::MinMaxResult;
 
-type Input = Vec<String>;
+type Input = Computer;
 type Output1 = usize;
 type Output2 = Output1;
 
-fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
-    input
-        .map(|line| {
-            let line = line.as_ref();
+fn parse<S: AsRef<str>, I: Iterator<Item = S>>(mut input: I) -> Input {
+    let line = input.next().unwrap();
 
-            todo!()
-        })
-        .collect()
+    Computer::parse(line.as_ref())
 }
 
-fn part_1(input: &Input) -> Output1 {
-    todo!()
+fn is_in_tractor_beam(computer: &Computer, x: i64, y: i64) -> bool {
+    let mut computer = computer.clone();
+    let mut io = VecDeque::new();
+
+    io.push_back(x);
+    io.push_back(y);
+
+    computer.run(&mut io);
+
+    io[0] == 1
+}
+
+fn part_1(computer: &Input) -> Output1 {
+    let mut in_tractor_beam = 0;
+    let mut min_x = 0;
+
+    for y in 0..5000 {
+        in_tractor_beam += match (min_x..5000)
+            .skip_while(|&x| !is_in_tractor_beam(computer, x, y))
+            .take_while(|&x| is_in_tractor_beam(computer, x, y))
+            .minmax()
+        {
+            MinMaxResult::NoElements => 0,
+            MinMaxResult::OneElement(x) => {
+                min_x = x;
+
+                1
+            }
+            MinMaxResult::MinMax(first_x, last_x) => {
+                min_x = first_x;
+
+                1usize + (last_x - first_x) as usize
+            }
+        }
+    }
+
+    in_tractor_beam
 }
 
 fn part_2(input: &Input) -> Output2 {
@@ -38,27 +73,4 @@ fn main() {
         let part_2 = log_run("Part 2", || part_2(&input));
         println!("Part 2: {part_2}");
     });
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const INPUT: &str = include_str!("test-input");
-
-    #[test]
-    fn test_part_1() {
-        let input = parse(INPUT.lines());
-        let result = part_1(&input);
-
-        assert_eq!(result, 0);
-    }
-
-    #[test]
-    fn test_part_2() {
-        let input = parse(INPUT.lines());
-        let result = part_2(&input);
-
-        assert_eq!(result, 0);
-    }
 }
