475d3b7ae12685c5453a4b5fbf5dab4739ed0907
Stefan Zwanenburg <stefan+git@zwanenburg.info>
2025-12-05 17:13:55 +0100

    2025-01 Added benches

--- years/2025/01/src/main.rs
+++ years/2025/01/src/main.rs
@@ -1,78 +1,6 @@
-use std::io;
-
+use aoc_2025_01::{parse, part_1, part_2};
 use aoc_timing::trace::log_run;
-
-type Input = Vec<Turn>;
-type Output1 = usize;
-type Output2 = Output1;
-
-enum Turn {
-    Left(u64),
-    Right(u64),
-}
-
-impl Turn {
-    fn to_number_to_add(&self) -> i32 {
-        match self {
-            Turn::Left(n) => -(*n as i32),
-            Turn::Right(n) => *n as i32,
-        }
-    }
-
-    fn to_clicks(&self) -> impl Iterator<Item = i32> {
-        let (click, n) = match self {
-            Turn::Left(n) => (-1, n),
-            Turn::Right(n) => (1, n),
-        };
-
-        (0..(*n as i32).abs()).map(move |_| click)
-    }
-}
-
-const DIAL_POSITIONS: i32 = 100;
-const DIAL_START_POSITION: i32 = 50;
-
-fn parse<S: AsRef<str>, I: Iterator<Item = S>>(input: I) -> Input {
-    input
-        .map(|line| {
-            let line = line.as_ref();
-            let (direction, count) = line.split_at(1);
-            let direction = direction.chars().next().unwrap();
-            let count = count.parse().expect("Invalid number of turns");
-
-            match direction {
-                'L' => Turn::Left(count),
-                'R' => Turn::Right(count),
-                _ => panic!("Unknown turn direction"),
-            }
-        })
-        .collect()
-}
-
-fn part_1(input: &Input) -> Output1 {
-    input
-        .iter()
-        .scan(DIAL_START_POSITION, |current_position, turn| {
-            *current_position += turn.to_number_to_add();
-            *current_position = current_position.rem_euclid(DIAL_POSITIONS);
-            Some(*current_position)
-        })
-        .filter(|&position| position == 0)
-        .count()
-}
-
-fn part_2(input: &Input) -> Output2 {
-    input
-        .iter()
-        .flat_map(|turn| turn.to_clicks())
-        .scan(DIAL_START_POSITION, |current_position, click| {
-            *current_position += click;
-            *current_position = current_position.rem_euclid(DIAL_POSITIONS);
-            Some(*current_position)
-        })
-        .filter(|&position| position == 0)
-        .count()
-}
+use std::io;
 
 fn main() {
     env_logger::init();
@@ -88,27 +16,4 @@ fn main() {
         let part_2 = log_run("Part 2", || part_2(&input));
         println!("Part 2: {part_2}");
     });
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const INPUT: &str = include_str!("test-input");
-
-    #[test]
-    fn test_part_1() {
-        let input = parse(INPUT.lines());
-        let result = part_1(&input);
-
-        assert_eq!(result, 3);
-    }
-
-    #[test]
-    fn test_part_2() {
-        let input = parse(INPUT.lines());
-        let result = part_2(&input);
-
-        assert_eq!(result, 6);
-    }
 }
